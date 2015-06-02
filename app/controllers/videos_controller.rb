b1e5b3baa5f60cb507c7518ab335c9fd30af5d91
Mike McCready <michaeljmccready@gmail.com>
2015-06-02 16:18:44 -0700

    functional index & new.html

---
+++ app/controllers/videos_controller.rb
@@ -0,0 +1,28 @@
+class VideosController < ApplicationController
+
+	def index
+		@videos = Video.all
+		
+	end
+
+	def new
+		@video = Video.new
+		
+	end
+
+	def create
+		@video = Video.new(video_params)
+			if @video.save
+				redirect_to "videos"
+			else	
+				render "new"
+			end
+	end
+
+	private
+		def video_params
+			params.require(:video).permit(:source,:title,:description)
+		end
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+
+  get "videos" => "videos#index"
+  get "videos/new" => "videos#new"
+  post "videos" => "videos#create"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
